b9a0b4337e70bb4dfce89c4dbbf568b3e4f32ed8
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-06-21 10:13:19 +0200

    Added destroy dependency

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates_presence_of :title, :description, :price
   validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
-  validates :price, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 
   def average_rating
   	self.reviews.average(:rating).try(:round, 2)
