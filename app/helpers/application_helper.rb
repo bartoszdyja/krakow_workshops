c9266eacc7d46b16301d2b4c6b90d0894e099e32
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-06-21 13:36:39 +0200

    Configured Devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
  
-
- 
-  def flash_messages()
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message) do 
-              concat message 
-            end)
-    end
-    nil
-  end
 end
 
